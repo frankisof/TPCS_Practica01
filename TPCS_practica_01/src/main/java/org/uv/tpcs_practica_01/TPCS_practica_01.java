8c049c73d73dd7ff74d8685c412dc9a8e2e95e1a
Francisco <fernandezfrancisco261@gmail.com>
2023-09-18 11:18:55 -0600

    primer ejemplo

---
+++ TPCS_practica_01/src/main/java/org/uv/tpcs_practica_01/TPCS_practica_01.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package org.uv.tpcs_practica_01;
+
+/**
+ *
+ * @author francisco
+ */
+public class TPCS_practica_01 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
